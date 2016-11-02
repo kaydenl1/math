6c20c2b7b029aa88b02a0b38f01a2aa233ba005a
klynch <klynch@newroads.org>
2016-11-02 11:45:22 -0400

    adding main

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+  
+  public static void main(Sting[] args){
+    
+  Scanner input = new Scanner(System.in);
+  
+  System.out.println("Inverse or Derivitive?");
+    
+  String in = input.nextLine();
+  
+    if (in.equals("Inverse")){
+      
+    }
+    
+  }
+}
