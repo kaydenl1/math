d999b33ba4caf539e1fb09920d36f09b147cedad
klynch <klynch@newroads.org>
2016-11-02 11:55:20 -0400

    This is updated

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
+import java.util.Scanner;
+
 public class Main{
   
   public static void main(Sting[] args){
     
   Scanner input = new Scanner(System.in);
   
-  System.out.println("Inverse or Derivitive?");
+  System.out.println("Inverse or Derivative?");
     
   String in = input.nextLine();
   
     if (in.equals("Inverse")){
-      
+      Inverse.desc();
     }
+    else if (in.equals("Derivative")){
     
   }
 }
