bb2873a1603a04306d92cebda9e8677b44c74136
jfeiner <jfeiner@newroads.org>
2016-11-02 12:27:49 -0400

    sasdfghjk

--- Derivative.java
+++ Derivative.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 class Derivative{
   
   public static void desc(){
     
-  
+  Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter problem f(x) = ");
+    
+    String in = input.nextLine(); 
     
   }
 }
